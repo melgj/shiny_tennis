a7e5f11e36f5bb39767692243e30f96258ddab04
melgj <cry-havoc22@hushmail.com>
2021-03-24 14:45:41 +0000

    add head to head function

--- app.R
+++ app.R
@@ -12,15 +12,16 @@ library(shinythemes)
 library(dplyr)
 library(data.table)
 
-matches <- fread("atp_results_db.csv")
+matches <- fread("matches.csv")
 matches$tourney_date <- lubridate::ymd(matches$tourney_date)
 matches$year <- lubridate::year(matches$tourney_date)
 matches <- matches %>% 
     select(year, tourney_date, tourney_name, round, best_of, winner_name, loser_name, score) %>%
-    filter(year >= 2000)
+    filter(year >= 2000) %>% 
+    arrange(desc(tourney_date))
 matches
-ratings <- fread("atp_SR_ratings_mar_2021.csv")
-ratings
+ratings <- fread("ratings.csv")
+ratings <- ratings %>% arrange(Player)
 playerList <- ratings$Player
 
 # Define UI for application that draws a histogram
@@ -29,26 +30,30 @@ ui <- fluidPage(
 
     # Application title
     titlePanel(
-        h1("Tennis Head to Head", align = "center")),
+        h1("Tennis Head to Head", align = "left")),
     
     fluidRow(
-        column(4,
-               dateRangeInput("dates", h4("Date Range"), start = "2020-01-01"),
+        column(4, 
+               dateRangeInput("dates", h4("Date Range"), start = "2000-01-01"),
                h4("Select Players"),
                selectInput(inputId = "player1", label = "Player 1", choices = playerList, selected = playerList[[1]]),
-               selectInput(inputId = "player2", label = "Player 2", choices = playerList, selected = playerList[[2]]),
-               dataTableOutput("h2h"))),
+               selectInput(inputId = "player2", label = "Player 2", choices = playerList, selected = playerList[[2]]))),
     fluidRow(
         column(4,
                h4("Get Elo Win Probabilities"),
-               actionButton("winProb", "Predict")))
+               actionButton("winProb", "Predict"))),
+    
+    fluidRow(
+    column(4, offset = 3,
+           dataTableOutput("h2h")))
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
     output$h2h <- renderDataTable({
         matches %>% dplyr::filter((winner_name == input$player1 & loser_name == input$player2) |
-                                      (loser_name == input$player1 & winner_name == input$player2))
+                                      (loser_name == input$player1 & winner_name == input$player2)) %>% 
+            filter(dplyr::between(tourney_date, input$dates[1], input$dates[2]))
     })
 }
 
