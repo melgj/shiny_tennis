21618db161e46025bd96d115d6e46f3bcef4826c
melgj <cry-havoc22@hushmail.com>
2021-03-25 16:57:13 +0000

    Remove default players from selections

--- app.R
+++ app.R
@@ -10,8 +10,8 @@ matches$tourney_date <- lubridate::ymd(matches$tourney_date)
 matches$year <- lubridate::year(matches$tourney_date)
 
 matches <- matches %>% 
-    select(year, tourney_date, tourney_name, surface, round, best_of, winner_name, loser_name, score) %>%
-    arrange(desc(tourney_date))
+    dplyr::select(year, tourney_date, tourney_name, surface, round, best_of, winner_name, loser_name, score) %>%
+    dplyr::arrange(desc(tourney_date))
 
 ratings <- fread("atp_ratings.csv")
 ratings <- ratings %>%
@@ -37,9 +37,11 @@ ui <- fluidPage(
         sidebarPanel(
                 dateRangeInput("dates", h4("Date Range Head-to-Head"), start = "2000-01-01"),
                 h4("Select Players"),
-                selectInput(inputId = "player1", label = "Player 1", choices = playerList, selected = playerList[[1]]),
-                selectInput(inputId = "player2", label = "Player 2", choices = playerList, selected = playerList[[2]]),
-                h4("Get Elo Win Probabilities"),
+                selectInput(inputId = "player1", label = "Player 1", 
+                            choices = c("Select Player" = "", playerList), selected = NULL),
+                selectInput(inputId = "player2", label = "Player 2", 
+                            choices = c("Select Player" = "", playerList), selected = NULL),
+                h4("Calculate Elo Win Probabilities"),
                 actionButton("winProb", "PREDICT"),
                 tableOutput("preds")),
         mainPanel(
@@ -56,16 +58,17 @@ server <- function(input, output) {
     output$winLoss <- renderTable({
         matches %>% dplyr::filter((winner_name == input$player1 & loser_name == input$player2) |
                                       (loser_name == input$player1 & winner_name == input$player2)) %>% 
-            filter(dplyr::between(tourney_date, input$dates[1], input$dates[2])) %>% 
-            group_by(winner_name) %>% 
-            summarise(Wins = n()) %>% 
-            rename(Player = winner_name) %>% 
-            arrange(desc(Wins))
+            dplyr::filter(dplyr::between(tourney_date, input$dates[1], input$dates[2])) %>% 
+            dplyr::group_by(winner_name) %>% 
+            dplyr::summarise(Wins = n()) %>% 
+            dplyr::rename(Player = winner_name) %>% 
+            dplyr::arrange(desc(Wins)) %>% 
+            dplyr::ungroup()
     })
     output$h2h <- renderDataTable({
         matches %>% dplyr::filter((winner_name == input$player1 & loser_name == input$player2) |
                                       (loser_name == input$player1 & winner_name == input$player2)) %>% 
-            filter(dplyr::between(tourney_date, input$dates[1], input$dates[2]))
+            dplyr::filter(dplyr::between(tourney_date, input$dates[1], input$dates[2]))
     })
     
     data <- eventReactive(input$winProb,{
