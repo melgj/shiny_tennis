897062a903a8afdfa5b94da2c296cb6c76ac9865
melgj <cry-havoc22@hushmail.com>
2021-03-26 06:34:21 +0000

    Change Elo function, add ratings history

--- app.R
+++ app.R
@@ -1,29 +1,51 @@
 
-library(data.table)
 library(shiny)
 library(shinythemes)
 library(dplyr)
+library(readr)
+library(PlayerRatings)
+library(lubridate)
 
 
-matches <- fread("atp_matches.csv")
+matches <- read_csv("atp_matches.csv")
 matches$tourney_date <- lubridate::ymd(matches$tourney_date)
-matches$year <- lubridate::year(matches$tourney_date)
+matches$Year <- lubridate::year(matches$tourney_date)
 
 matches <- matches %>% 
-    dplyr::select(year, tourney_date, tourney_name, surface, round, best_of, winner_name, loser_name, score) %>%
-    dplyr::arrange(desc(tourney_date))
+    dplyr::select(Year, tourney_date, tourney_name, surface, round, best_of, winner_name, loser_name, score) %>%
+    dplyr::rename(`Event Date` = tourney_date,
+                  `Event Name` = tourney_name,
+                  Surface = surface,
+                  Round = round,
+                  `Best Of` = best_of,
+                  Winner = winner_name,
+                  Loser = loser_name,
+                  `Match Score` = score) 
 
-ratings <- fread("atp_ratings.csv")
-ratings <- ratings %>%
-    dplyr::select(Player, Rating) %>% 
-    dplyr::arrange(Player)
+rndLevels <- c("R128","R64","R32","R16","RR","QF","SF","BR","F")
+
+matches$Round <- factor(matches$Round, rndLevels, ordered = T)
+
+matches <- matches %>% 
+    dplyr::arrange(desc(`Event Date`), desc(Round))
+
+Base_Date <- min(matches$`Event Date`)
+End_Date <- max(matches$`Event Date`)
+
+match_hist <- matches %>% 
+    mutate(Period = lubridate::interval(Base_Date, matches$`Event Date`) %/% weeks(1)+1,
+           Result = 1) %>% 
+    arrange(`Event Date`, Round) %>% 
+    select(Period, Winner, Loser, Result)
+
+sElo <- steph(match_hist, init = c(1500,300), history = TRUE)
+
+ratings <- as_tibble(sElo$ratings) %>% 
+    filter(Lag <= 25, Rating >= 1500, Games >= 30)
+
+ratings
 
 playerList <- ratings$Player
-
-probP1 <- function(p1_rtng, p2_rtng){
-    dij <- p2_rtng - p1_rtng
-    return(1 / (1 + 10 ^ (dij/400)))
-}
 
 ui <- fluidPage(
     theme = shinytheme("cerulean"),
@@ -34,7 +56,8 @@ ui <- fluidPage(
     
     sidebarLayout(
         sidebarPanel(
-                dateRangeInput("dates", h4("Date Range Head-to-Head"), start = "2000-01-01"),
+                dateRangeInput("dates", h4("Date Range Head-to-Head"),
+                               start = "2000-01-01", end = End_Date),
                 h4("Select Players"),
                 selectInput(inputId = "player1", label = "Player 1", 
                             choices = playerList, selected = playerList[[1]]),
@@ -55,35 +78,42 @@ ui <- fluidPage(
 
 server <- function(input, output) {
     output$winLoss <- renderTable({
-        matches %>% dplyr::filter((winner_name == input$player1 & loser_name == input$player2) |
-                                      (loser_name == input$player1 & winner_name == input$player2)) %>% 
-            dplyr::filter(dplyr::between(tourney_date, input$dates[1], input$dates[2])) %>% 
-            group_by(winner_name) %>% 
+        matches %>% dplyr::filter((Winner == input$player1 & Loser == input$player2) |
+                                      (Loser == input$player1 & Winner == input$player2)) %>% 
+            dplyr::filter(dplyr::between(`Event Date`, input$dates[1], input$dates[2])) %>% 
+            group_by(Winner) %>% 
             summarise(Wins = n()) %>% 
-            rename(Player = winner_name) %>% 
+            rename(Player = Winner) %>% 
             arrange(desc(Wins))
         
     })
     output$h2h <- renderDataTable({
-        matches %>% dplyr::filter((winner_name == input$player1 & loser_name == input$player2) |
-                                      (loser_name == input$player1 & winner_name == input$player2)) %>% 
-            dplyr::filter(dplyr::between(tourney_date, input$dates[1], input$dates[2])) %>% 
-            dplyr::rename(Match_Score = score,
-                          Winner = winner_name,
-                          Loser = loser_name)
+        matches %>% dplyr::filter((Winner == input$player1 & Loser == input$player2) |
+                                      (Loser == input$player1 & Winner == input$player2)) %>% 
+            dplyr::filter(dplyr::between(`Event Date`, input$dates[1], input$dates[2]))
+            
     })
     
     data <- eventReactive(input$winProb,{
+        
+        matchup <- tibble(Time = 1,
+                           Player1 = input$player1,
+                           Player2 = input$player2)
+        
+        predProb <- predict(sElo, matchup, trat = c(1500, 300), gamma = 0)
+        
+        matchPred <- tibble(Player = c(input$player1, input$player2),
+                                `Win Probability` = c(round(predProb, 2), round(1 - predProb, 2)))
     
-                p1Elo <- round(ratings$Rating[ratings$Player == input$player1],2)
-                p2Elo <- round(ratings$Rating[ratings$Player == input$player2],2)
-                
-                p1Prob <- probP1(p1Elo, p2Elo)
-                p2Prob <- 1 - p1Prob
-                
-                probTable <- data.table(Player = c(input$player1, input$player2),
-                                        `Elo Rating` = c(p1Elo, p2Elo),
-                                        `Win Probability` = c(p1Prob, p2Prob))
+                # p1Elo <- round(ratings$Rating[ratings$Player == input$player1],2)
+                # p2Elo <- round(ratings$Rating[ratings$Player == input$player2],2)
+                # 
+                # p1Prob <- probP1(p1Elo, p2Elo)
+                # p2Prob <- 1 - p1Prob
+                # 
+                # probTable <- data.table(Player = c(input$player1, input$player2),
+                #                         `Elo Rating` = c(p1Elo, p2Elo),
+                #                         `Win Probability` = c(p1Prob, p2Prob))
     })
     
     output$preds <- renderTable({
