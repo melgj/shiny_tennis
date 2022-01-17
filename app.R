c3d97f57398cf71ec7261aabba1e20352e384a53
melgj <cry-havoc22@hushmail.com>
2022-01-17 06:29:01 +0000

    update end 2021

--- app.R
+++ app.R
@@ -8,9 +8,9 @@ library(ggplot2)
 
 
 
-atpHistory <- read_csv("atp_shiny_timeline_2021-09-15.csv", col_names = T)
-atpRatings <- read_csv("atp_SR_ratings_2021-09-15.csv", col_names = T)
-matches <- read_csv("atp_shiny_matches_2021-09-15.csv", col_names = TRUE)
+atpHistory <- read_csv("~/atp/atp_shiny_timeline.csv", col_names = T)
+atpRatings <- read_csv("~/atp/atp_SR_ratings.csv", col_names = T)
+matches <- read_csv("~/atp/atp_shiny_matches.csv", col_names = TRUE)
 
 atpHistory
 atpRatings$Rating <- round(atpRatings$Rating, 2)
